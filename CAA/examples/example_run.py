79af2cef19a43977c809ab460e4b6564bd809b05
mathbarre <mathieu.barre83@gmail.com>
2021-04-09 23:58:43 +0200

    correct file saving

--- CAA/examples/example_run.py
+++ CAA/examples/example_run.py
@@ -16,7 +16,8 @@ from CAA.utils.utils import power_method
 # X, y = fetch_libsvm("liver-disorders", normalize=False) # good
 # X, y = fetch_libsvm("phishing", normalize=False) # good
 
-datasets = ["german.numer", "colon-cancer", "madelon", "madelon_test", "rcv1.binary", "rcv1.binary_test", "real-sim", "leukemia", "leukemia_test"]
+datasets = ["colon-cancer", "madelon", "madelon_test", "rcv1.binary",
+            "rcv1.binary_test", "real-sim", "leukemia", "leukemia_test"]
 conditionings = [1e-6, 1e-7, 1e-8, 1e-9]
 
 for dataset in datasets:
@@ -43,10 +44,11 @@ for dataset in datasets:
         all_Es = {}
         all_Ts = {}
 
-        fgap = 1_000
+        fgap = 500
         # max_iter = 150_001
         max_iter = 1_000_001
         verbose = True
+        n = int(np.floor(max_iter/fgap))+1
 
         for algo in all_algos:
             algo_name = algo[0]
@@ -63,9 +65,10 @@ for dataset in datasets:
                 X, y, rho=conditioning*L, verbose=False,
                 tol=tol, C0=C, adaptive_C=True, use_acc=use_acc,
                 max_iter=max_iter*iters, f_grad=fgap, K=5, reg_amount=reg)
-            print("%s --- %s seconds ---" % (algo_name, time.time() - start_time))
-            all_Es[algo] = E
-            all_Ts[algo] = T
+            print("%s --- %s seconds ---" % (algo_name, time.time() -
+                                             start_time))
+            all_Es[algo] = np.hstack([np.array(E), np.zeros(n-len(E))])
+            all_Ts[algo] = np.hstack([np.array(T), np.zeros(n-len(T))])
 
         res = np.array([])
         for algo in all_algos:
@@ -76,5 +79,6 @@ for dataset in datasets:
             res = np.vstack([res, all_Ts[algo]])
         res = np.vstack([res, fgap * np.arange(res.shape[1])])
 
-        name = "./CAA/examples/results/logreg_%s_C0_%i_rho_%e.txt" % (dataset, C0, conditioning)
+        name = ("./CAA/examples/results/logreg_%s_C0_%i_rho_%e.txt" %
+                (dataset, C0, conditioning))
         np.savetxt(name, res.T)
